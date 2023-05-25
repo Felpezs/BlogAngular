6a73da77e76ab20576314ae50d7a6467988a9594
Felipe <felipe.edfreire@gmail.com>
2023-05-25 18:21:30 -0300

    feat: adicionando rotas e uso de dados (json)

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -3,17 +3,14 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
-  styleUrls: ['./big-card.component.css']
+  styleUrls: ['./big-card.component.css'],
 })
 export class BigCardComponent implements OnInit {
+  @Input() photoCover: string = '';
+  @Input() cardTitle: string = '';
+  @Input() cardDescription: string = '';
+  @Input() Id: string = '';
+  constructor() {}
 
-  @Input() photoCover: string = ""
-  @Input() cardTitle: string = ""
-  @Input() cardDescription: string = ""
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
